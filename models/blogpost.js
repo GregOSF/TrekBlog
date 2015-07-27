32649b3752f2fb12a791db71c9086367832996dc
GregOSF <gregory.onza@gmail.com>
2015-07-27 11:11:19 -0700

    modify blogpost schema

--- models/blogpost.js
+++ models/blogpost.js
@@ -1,12 +1,16 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
+
+// Make variable for schema
 var BlogSchema = new Schema ({
 	user: String,
 	place: String,
 	post: String
 });
 
+// First parameter is name of the collection in Mongo
 var BlogPost = mongoose.model('posts', BlogSchema);
+
 
 module.exports = BlogPost;
